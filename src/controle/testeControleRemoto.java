5ca8a3f9e6aa06b206c1f801daab91d092531f5b
xluary <isabellabarral@hotmail.com>
2022-10-24 15:59:42 -0300

    Exercício de encapsulamento

---
+++ src/controle/testeControleRemoto.java
@@ -0,0 +1,10 @@
+package controle;
+
+public class testeControleRemoto {
+	public static void main(String [] args) {
+		ControleRemoto c= new ControleRemoto();
+		c.ligar();	
+		c.maisVolume();
+		c.abrirMenu();
+	}
+}
